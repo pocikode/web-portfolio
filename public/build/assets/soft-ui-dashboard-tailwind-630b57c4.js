fc9a809a6ccf99df889eddd2b95ba704f3184383
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2023-11-05 05:24:35 +0700

    include asset build in git

---
+++ public/build/assets/soft-ui-dashboard-tailwind-630b57c4.js
@@ -0,0 +1,17 @@
+/*!
+
+=========================================================
+* Soft UI Dashboard Tailwind - v1.0.5
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-tailwind
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (site.license)
+
+* Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/window.location.pathname.split("/").pop().split(".")[0];var i=window.location.pathname.split("/"),e=i.includes("pages")?"../":"./";window.location.pathname.split("/");i.includes("pages");c(e+"assets/css/perfect-scrollbar.css");t(e+"assets/js/perfect-scrollbar.js",!0);document.querySelector("nav [navbar-trigger]")&&t(e+"assets/js/navbar-collapse.js",!0);document.querySelector("[data-target='tooltip']")&&(t(e+"assets/js/tooltips.js",!0),c(e+"assets/css/tooltips.css"));document.querySelector("[nav-pills]")&&t(e+"assets/js/nav-pills.js",!0);document.querySelector("[dropdown-trigger]")&&t(e+"assets/js/dropdown.js",!0);document.querySelector("[fixed-plugin]")&&t(e+"assets/js/fixed-plugin.js",!0);document.querySelector("[navbar-main]")&&(t(e+"assets/js/sidenav-burger.js",!0),t(e+"assets/js/navbar-sticky.js",!0));document.querySelector("canvas")&&(t(e+"assets/js/chart-1.js",!0),t(e+"assets/js/chart-2.js",!0));function t(r,s){let a=document.createElement("script");a.setAttribute("src",r),a.setAttribute("type","text/javascript"),a.setAttribute("async",s),document.head.appendChild(a)}function c(r){let s=document.createElement("link");s.setAttribute("href",r),s.setAttribute("type","text/css"),s.setAttribute("rel","stylesheet"),document.head.appendChild(s)}
